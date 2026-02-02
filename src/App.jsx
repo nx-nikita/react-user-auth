a0ad286cdb038854feab40cacc8812ab66f5aeaa
nx-nikita <niti81677@gmail.com>
2026-02-02 12:35:34 +0530

    Fix double router issue for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Profile from "./pages/Profile";
-import Signin from "./pages/Signin"; // ✅ FIXED
+import Signin from "./pages/Signin";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/profile" element={<Profile />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signin" element={<Signin />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
   );
 }
 
